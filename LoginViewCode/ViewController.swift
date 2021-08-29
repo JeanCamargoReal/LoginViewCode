29c282b71124bbde0f99ebca628d4b6fe7ccf331
JeanCamargoReal <jeancamargoreal@icloud.com>
2021-08-29 00:08:44 -0300

    Configurando projeto para aceitar ViewCode

---
+++ LoginViewCode/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LoginViewCode
+//
+//  Created by Jean Paulo Marcel Henrique de Camargo on 28/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .red
+    }
+
+
+}
+
