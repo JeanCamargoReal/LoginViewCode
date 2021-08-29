636caa1be73c0503f615b843607f6161937551a1
JeanCamargoReal <jeancamargoreal@icloud.com>
2021-08-29 14:39:02 -0300

    refatorando projeto

---
+++ LoginViewCode/Login/VC/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  LoginViewCode
+//
+//  Created by Jean Paulo Marcel Henrique de Camargo on 28/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var loginScreen: LoginScreen?
+    
+    override func loadView() {
+        self.loginScreen = LoginScreen()
+        self.view = self.loginScreen
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .red
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        self.navigationController?.setNavigationBarHidden(true, animated: false)
+    }
+
+}
+
