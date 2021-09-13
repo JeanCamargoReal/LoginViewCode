347c3063b6602ab0b4dcfb931ec8a07309dc930f
JeanCamargoReal <jeancamargoreal@icloud.com>
2021-09-13 14:20:14 -0300

    Adicionando botão e imagem de voltar na tela de register; adicionando imagem de adicionar usuário na tela de register

---
+++ LoginViewCode/Register/Screen/RegisterScreen.swift
@@ -0,0 +1,59 @@
+//
+//  RegisterScreen.swift
+//  LoginViewCode
+//
+//  Created by Jean Camargo on 13/09/21.
+//
+
+import UIKit
+
+class RegisterScreen: UIView {
+    
+    lazy var backButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setImage(UIImage(named: "back"), for: .normal)
+        return button
+    }()
+    
+    lazy var imageAddUser: UIImageView = {
+        let image = UIImageView()
+        image.translatesAutoresizingMaskIntoConstraints = false
+        image.image = UIImage(named: "usuario")
+        image.contentMode = .scaleAspectFit
+        image.tintColor = .white
+        return image
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.configBackground()
+        self.configSuperView()
+        self.setUpConstraints()
+    }
+    
+    private func configSuperView() {
+        self.addSubview(self.backButton)
+        self.addSubview(self.imageAddUser)
+    }
+    
+    private func configBackground() {
+        self.backgroundColor = UIColor(red: 24/255, green: 115/255, blue: 104/255, alpha: 1.0)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setUpConstraints() {
+        NSLayoutConstraint.activate([
+            self.imageAddUser.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor, constant: 20),
+            self.imageAddUser.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+            self.imageAddUser.widthAnchor.constraint(equalToConstant: 150),
+            self.imageAddUser.heightAnchor.constraint(equalToConstant: 150),
+            
+            self.backButton.topAnchor.constraint(equalTo: self.imageAddUser.topAnchor),
+            self.backButton.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 20)
+        ])
+    }
+}
