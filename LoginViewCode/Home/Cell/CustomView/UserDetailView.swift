539bc540d6b6e3918faa49e66b5f3520db62fc83
JeanCamargoReal <jeancamargoreal@icloud.com>
2021-09-27 15:48:29 -0300

    Refatorando e adicionando cell customizada

---
+++ LoginViewCode/Home/Cell/CustomView/UserDetailView.swift
@@ -0,0 +1,57 @@
+//
+//  UserDetailView.swift
+//  LoginViewCode
+//
+//  Created by Jean Camargo on 27/09/21.
+//
+
+import UIKit
+
+class UserDetailView: UIView {
+    
+    lazy var userImageView: UIImageView = {
+        let image = UIImageView()
+        image.translatesAutoresizingMaskIntoConstraints = false
+        image.contentMode = .scaleAspectFit
+        
+        return image
+    }()
+    
+    lazy var nameLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .black
+        
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.addSubView()
+        self.configUserImageViewConstraints()
+        self.configNameLabelConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func addSubView() {
+        self.addSubview(self.userImageView)
+        self.addSubview(self.nameLabel)
+    }
+    
+    func configUserImageViewConstraints() {
+        self.userImageView.snp.makeConstraints { make in
+            make.centerY.leading.equalToSuperview()
+            make.height.width.equalTo(80)
+        }
+    }
+    
+    func configNameLabelConstraints() {
+        self.nameLabel.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.leading.equalTo(self.userImageView.snp.trailing).offset(20)
+        }
+    }
+}
